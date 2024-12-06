8a6450f1b3d216e1b22425a3642f2d5d63145e22
Tyoma0 <artyom0070071@mail.ru>
2024-12-06 13:33:39 +0300

    g

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Header } from './layout/header/Header';
 import { Main } from './layout/sections/main/Main';
 import { Skills } from './layout/sections/skills/Skills';
 import { Works } from './layout/sections/works/Works';
+import { AboutMe } from './layout/aboutMe/AboutMe';
+import { Contact } from './layout/contact/Contact';
 
 function App() {
     return (
@@ -12,6 +14,8 @@ function App() {
           <Main/>
           <Skills/>
           <Works/>
+          <AboutMe/>
+          <Contact/>
         </div>
     );
 }
